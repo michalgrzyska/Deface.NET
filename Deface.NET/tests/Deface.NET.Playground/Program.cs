8a27376802e72cbf919232f6fdd234c2a137abd3
Michał Grzyśka <grzyska.michal@gmail.com>
2024-11-17 19:24:10 +0100

    Update Program.cs

--- Deface.NET/tests/Deface.NET.Playground/Program.cs
+++ Deface.NET/tests/Deface.NET.Playground/Program.cs
@@ -5,20 +5,18 @@ Console.WriteLine(Directory.GetCurrentDirectory());
 
 IDefaceService defaceService = DefaceProvider.GetDefaceService(options =>
 {
-    //options.FFMpegPath = "C://ffmpeg//ffmpeg.exe";
-    //options.FFProbePath = "C://ffmpeg//ffprobe.exe";
+    options.FFMpegPath = "C://ffmpeg//ffmpeg.exe";
+    options.FFProbePath = "C://ffmpeg//ffprobe.exe";
 
-    options.FFMpegPath = "/usr/bin/ffmpeg";
-    options.FFProbePath = "/usr/bin/ffprobe";
+    //options.FFMpegPath = "/usr/bin/ffmpeg";
+    //options.FFProbePath = "/usr/bin/ffprobe";
 
     options.AnonimizationMethod = AnonimizationMethod.Mosaic;
-    //options.RunDetectionEachNFrames = 3;
     options.AnonimizationShape = AnonimizationShape.Ellipse;
     options.ImageFormat = ImageFormat.Jpeg(10);
 });
 
-//var result = await defaceService.ProcessVideo(TestResources.Video_Short_640_360_24fps, "testt.mp4");
-var result = await defaceService.ProcessVideo("/mnt/c/Users/mihax/OneDrive/Documents/GitHub/Deface.NET/Deface.NET/tests/Deface.NET.Playground/bin/Debug/net8.0/Res/Videos/short_640_360_24fps.mp4", "testt.mp4");
+var result = await defaceService.ProcessVideo(TestResources.Video_Short_640_360_24fps, "testt.mp4");
 
 //var result = defaceService.ProcessImage(TestResources.Photo1, "C://DefaceTest//1.png", x => { x.AnonimizationShape = AnonimizationShape.Rectangle; });
 
