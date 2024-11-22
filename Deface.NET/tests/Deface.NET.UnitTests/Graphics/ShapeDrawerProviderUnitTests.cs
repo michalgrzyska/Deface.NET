ae34f11e7ec934d296e29af6154105d9c11dfc9a
Michał Grzyśka <grzyska.michal@gmail.com>
2024-11-22 18:51:56 +0100

    Create ShapeDrawerProviderUnitTests.cs

---
+++ Deface.NET/tests/Deface.NET.UnitTests/Graphics/ShapeDrawerProviderUnitTests.cs
@@ -0,0 +1,56 @@
+﻿using Deface.NET.Configuration.Provider;
+using Deface.NET.Graphics;
+using Deface.NET.Graphics.Effects;
+using Deface.NET.UnitTests._TestsConfig;
+using NSubstitute;
+
+namespace Deface.NET.UnitTests.Graphics;
+
+[Collection(nameof(SettingsCollection))]
+public class ShapeDrawerProviderUnitTests(SettingsFixture settingsFixture)
+{
+    private readonly SettingsFixture _settingsFixture = settingsFixture;
+
+    [Fact]
+    public void ShapeDrawer_ShouldBeGaussianBlurShapeDrawer()
+    {
+        var settingsProvider = GetSettingsProvider(AnonimizationMethod.GaussianBlur);
+
+        ShapeDrawerProvider provider = new(settingsProvider);
+
+        provider.ShapeDrawer.Should().BeAssignableTo<GaussianBlurShapeDrawer>();
+    }
+
+    [Fact]
+    public void ShapeDrawer_ShouldBeMosaicShapeDrawer()
+    {
+        var settingsProvider = GetSettingsProvider(AnonimizationMethod.Mosaic);
+
+        ShapeDrawerProvider provider = new(settingsProvider);
+
+        provider.ShapeDrawer.Should().BeAssignableTo<MosaicShapeDrawer>();
+    }
+
+    [Fact]
+    public void ShapeDrawer_ShouldBeColorShapeDrawer()
+    {
+        var settingsProvider = GetSettingsProvider(AnonimizationMethod.Color(0, 0, 0));
+
+        ShapeDrawerProvider provider = new(settingsProvider);
+
+        provider.ShapeDrawer.Should().BeAssignableTo<ColorShapeDrawer>();
+    }
+
+    private IScopedSettingsProvider GetSettingsProvider(AnonimizationMethod method)
+    {
+        var settings = _settingsFixture.WithAction(x =>
+        {
+            x.AnonimizationMethod = method;
+        });
+
+        var settingsProvider = Substitute.For<IScopedSettingsProvider>();
+        settingsProvider.Settings.Returns(settings);
+
+        return settingsProvider;
+    }
+}
