1a3375f45459bd67b2cb387c530964be0e921159
Michał Grzyśka <grzyska.michal@gmail.com>
2025-02-03 22:40:43 +0100

    WiP

---
+++ Deface.NET/tests/Deface.NET.IntegrationTests/Helpers/IOTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Deface.NET.IntegrationTests.Helpers;
+
+public abstract class IOTest
+{
+    public static void Cleanup(params string[] paths)
+    {
+        foreach (var path in paths)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
--- Deface.NET/tests/Deface.NET.IntegrationTests/ImageFormatTests.cs
+++ Deface.NET/tests/Deface.NET.IntegrationTests/ImageFormatTests.cs
@@ -2,65 +2,93 @@
 
 namespace Deface.NET.IntegrationTests;
 
-public class ImageFormatTests
+public class ImageFormatTests : IOTest
 {
     [Fact]
     public void ProcessImage_SettingPngFormat_ShouldSaveFileAsPng()
     {
-        var deface = DefaceProvider.GetDefaceService(options =>
+        ProcessingResult result = default;
+
+        try
         {
-            options.ImageFormat = ImageFormat.Png;
-        });
+            var deface = DefaceProvider.GetDefaceService(options =>
+            {
+                options.ImageFormat = ImageFormat.Png;
+            });
 
-        var result = deface.ProcessImage(TestResources.TestResources.Photo1, "output.png");
+            result = deface.ProcessImage(TestResources.TestResources.Photo1, "output.png");
 
-        Path.GetExtension(result.OutputFile).ShouldBe(".png");
-
-        File.Delete(result.OutputFile);
+            Path.GetExtension(result.OutputFile).ShouldBe(".png");
+        }
+        finally
+        {
+            Cleanup(result?.OutputFile);
+        }
     }
 
     [Fact]
     public void ProcessImage_SettingJpegFormat_ShouldSaveFileAsJpeg()
     {
-        var deface = DefaceProvider.GetDefaceService(options =>
+        ProcessingResult result = default;
+
+        try
         {
-            options.ImageFormat = ImageFormat.Jpeg();
-        });
+            var deface = DefaceProvider.GetDefaceService(options =>
+            {
+                options.ImageFormat = ImageFormat.Jpeg();
+            });
 
-        var result = deface.ProcessImage(TestResources.TestResources.Photo1, "output.jpg");
+            result = deface.ProcessImage(TestResources.TestResources.Photo1, "output.jpg");
 
-        Path.GetExtension(result.OutputFile).ShouldBe(".jpg");
-
-        File.Delete(result.OutputFile);
+            Path.GetExtension(result.OutputFile).ShouldBe(".jpg");
+        }
+        finally
+        {
+            Cleanup(result?.OutputFile);
+        }
     }
 
     [Fact]
     public void ProcessImage_SettingPngFormat_TempPngFileShouldExist()
     {
-        var deface = DefaceProvider.GetDefaceService(options =>
+        ProcessingResult result = default;
+
+        try
         {
-            options.ImageFormat = ImageFormat.Png;
-        });
+            var deface = DefaceProvider.GetDefaceService(options =>
+            {
+                options.ImageFormat = ImageFormat.Png;
+            });
 
-        var result = deface.ProcessImage(TestResources.TestResources.Photo2, "output.png");
+            result = deface.ProcessImage(TestResources.TestResources.Photo2, "output.png");
 
-        File.Exists(result.OutputFile).ShouldBeTrue();
-
-        File.Delete(result.OutputFile);
+            File.Exists(result.OutputFile).ShouldBeTrue();
+        }
+        finally
+        {
+            Cleanup(result?.OutputFile);
+        }
     }
 
     [Fact]
     public void ProcessImage_SettingPngFormat_TempJpegFileShouldExist()
     {
-        var deface = DefaceProvider.GetDefaceService(options =>
+        ProcessingResult result = default;
+
+        try
         {
-            options.ImageFormat = ImageFormat.Jpeg();
-        });
+            var deface = DefaceProvider.GetDefaceService(options =>
+            {
+                options.ImageFormat = ImageFormat.Jpeg();
+            });
 
-        var result = deface.ProcessImage(TestResources.TestResources.Photo2, "output.jpg");
+            result = deface.ProcessImage(TestResources.TestResources.Photo2, "output.jpg");
 
-        File.Exists(result.OutputFile).ShouldBeTrue();
-
-        File.Delete(result.OutputFile);
+            File.Exists(result.OutputFile).ShouldBeTrue();
+        }
+        finally
+        {
+            Cleanup(result?.OutputFile);
+        }
     }
 }
