74e9b62ab58ffad5fea9c0b1ace51b211873c3e0
Cube <wieszalajakub@gmail.com>
2025-02-03 18:21:05 +0100

    backupp

--- Deface.NET/tests/Deface.NET.Playground/Program.cs
+++ Deface.NET/tests/Deface.NET.Playground/Program.cs
@@ -5,14 +5,13 @@ Console.WriteLine(Directory.GetCurrentDirectory());
 
 IDefaceService defaceService = DefaceProvider.GetDefaceService(options =>
 {
+    options.AnonimizationMethod = AnonimizationMethod.Mosaic;
     options.AnonimizationShape = AnonimizationShape.Ellipse;
+    options.Hardware = Hardware.Cuda(0);
     options.EncodingCodec = EncodingCodec.H264;
-    options.FFMpegPath = "C://ffmpeg//ffmpeg.exe";
-    options.FFProbePath = "C://ffmpeg//ffprobe.exe";
-    options.AnonimizationMethod = AnonimizationMethod.Color(255, 0, 0);
 });
 
-var result = defaceService.ProcessVideo(TestResources.Video_Short_640_360_24fps, "\"C://DefaceTest//testt");
+var result = defaceService.ProcessVideo(TestResources.Video_Very_Short_480p, "\"C://DefaceTest//testt");
 
 //var result = defaceService.ProcessImage(TestResources.Photo3, "C://DefaceTest//4.png");
 
